a6870e04acc8483870c572c66ba944c9c9c21efe
paria <83511327+sasank-paria@users.noreply.github.com>
2023-07-07 15:26:44 +0530

    .

---
+++ dsa python/27pascal_triangle.py
@@ -0,0 +1,33 @@
+'''
+suppose N = 5
+output:
+
+1
+1 1
+1 2 1
+1 3 3 1
+1 4 6 4 1
+
+Here for the third row, you will see that the second element is the summation of the
+above two-row elements i.e. 2=1+1, and similarly for row three 3 = 1+2 and 3 = 1+2.
+'''
+
+# to solve this assume zeroes at the ending and beginneing of the rows
+
+no_rows = 5
+
+res = [[1]]
+
+for i in range((no_rows)-1):
+    temp = [0]+ res[-1]+[0]
+    rows = []
+    for j in range(len(res[-1])+1):
+        rows.append(temp[j]+temp[j+1])
+    res.append(rows)
+
+print(res)
+
+for x in res:
+    for y in x:
+        print(y,end=" ")
+    print()
