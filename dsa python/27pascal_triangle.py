9147ff09cec0aa31d440378a6592b90d79d765c9
paria <83511327+sasank-paria@users.noreply.github.com>
2023-07-08 01:47:55 +0530

    .

--- dsa python/27pascal_triangle.py
+++ dsa python/27pascal_triangle.py
@@ -8,7 +8,7 @@ output:
 1 3 3 1
 1 4 6 4 1
 
-Here for the third row, you will see that the second element is the summation of the
+Here for the third row, you will see that the second element is the summation of the 
 above two-row elements i.e. 2=1+1, and similarly for row three 3 = 1+2 and 3 = 1+2.
 '''
 
@@ -18,16 +18,11 @@ no_rows = 5
 
 res = [[1]]
 
-for i in range((no_rows)-1):
-    temp = [0]+ res[-1]+[0]
+for i in range(len(no_rows)+1):
+    temp = [0]+ res[-1]+res[0]
     rows = []
     for j in range(len(res[-1])+1):
         rows.append(temp[j]+temp[j+1])
     res.append(rows)
 
 print(res)
-
-for x in res:
-    for y in x:
-        print(y,end=" ")
-    print()
