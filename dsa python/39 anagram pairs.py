0da8030847b1a9b7ebb040646eef4073fdc66084
paria <83511327+sasank-paria@users.noreply.github.com>
2023-07-20 21:24:10 +0530

    .

---
+++ dsa python/39 anagram pairs.py
@@ -0,0 +1,15 @@
+'''
+xample 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+'''
+
+# using hash map #
+
+s = "anagram"
+t = "nagaram"
